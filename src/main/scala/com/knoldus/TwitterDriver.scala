25e1d07e3cbd11e1e087601d76b8c3845ef0df9b
mansi7babbar <mansi7babbar@gmail.com>
2020-03-02 15:15:59 +0530

    updated future-api assignment

--- src/main/scala/com/knoldus/TwitterDriver.scala
+++ src/main/scala/com/knoldus/TwitterDriver.scala
@@ -1,7 +1,7 @@
 package com.knoldus
 
 object TwitterDriver extends App {
-  val retrievedTweets = TwitterOps.retrieveTweet("sunday")
+  val retrievedTweets = TwitterOps.retrieveTweet("#sunday")
   val numberOfTweets = TwitterOps.getNumberOfTweets(retrievedTweets)
   val averageLikesPerTweet = TwitterOps.getAverageLikesPerTweet(retrievedTweets)
   val averageRetweetsPerTweet = TwitterOps.getAverageRetweetsPerTweet(retrievedTweets)

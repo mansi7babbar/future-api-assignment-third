3035735090e249c78ac0de528fac7c9213a77e1b
mansi7babbar <mansi7babbar@gmail.com>
2020-03-06 10:34:23 +0530

    added twitter case class

--- src/main/scala/com/knoldus/TwitterOps.scala
+++ src/main/scala/com/knoldus/TwitterOps.scala
@@ -2,6 +2,7 @@ package com.knoldus
 
 import java.sql.Timestamp
 import java.time.Duration
+import java.util.Date
 
 import twitter4j._
 
@@ -9,42 +10,53 @@ import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+case class CustomTweet(id: Long, favoriteCount: Int, reTweetCount: Int, createdAt: Date)
+
 object TwitterOps extends TwitterSetup {
 
-  def retrieveTweet(hashTag: String): Future[List[Status]] = Future {
+  def retrieveTweet(hashTag: String): Future[List[CustomTweet]] = Future {
     val hashTagQuery = new Query(hashTag)
     val retrievedTweets = twitter.search(hashTagQuery).getTweets.asScala.toList
-    retrievedTweets
-  }
+    retrievedTweets.map(retrievedTweet => CustomTweet(retrievedTweet.getId, retrievedTweet.getFavoriteCount,
+      retrievedTweet.getRetweetCount, retrievedTweet.getCreatedAt))
+  }.fallbackTo(Future {
+    -1
+  })
 
-  def getNumberOfTweets(tweets: Future[List[Status]]): Future[Int] = {
+  def getNumberOfTweets(tweets: Future[List[CustomTweet]]): Future[Int] = {
     tweets.map { tweets =>
       tweets.length
     }
-  }
+  }.fallbackTo(Future {
+    -1
+  })
 
-  def getAverageLikesPerTweet(tweets: Future[List[Status]]): Future[Int] = {
+  def getAverageLikesPerTweet(tweets: Future[List[CustomTweet]]): Future[Int] = {
     tweets.map { tweets =>
       val getFavouriteCount = tweets.foldLeft(0) { (sum, tweet) =>
-        sum + tweet.getFavoriteCount
+        sum + tweet.favoriteCount
       }
       getFavouriteCount / tweets.length
     }
-  }
+  }.fallbackTo(Future {
+    -1
+  })
 
-  def getAverageRetweetsPerTweet(tweets: Future[List[Status]]): Future[Int] = {
+  def getAverageRetweetsPerTweet(tweets: Future[List[CustomTweet]]): Future[Int] = {
     tweets.map { tweets =>
       val getRetweetCount = tweets.foldLeft(0) { (sum, tweet) =>
-        sum + tweet.getRetweetCount
+        sum + tweet.reTweetCount
       }
       getRetweetCount / tweets.length
     }
-  }
+  }.fallbackTo(Future {
+    -1
+  })
 
-  def getAverageTweetsPerDay(posts: Future[List[Status]]): Future[Long] = {
-    val sortedPosts = posts.map(post => post.sortWith((a, b) => a.getCreatedAt.before(b.getCreatedAt)))
-    val start = sortedPosts.map(sortedPost => sortedPost.head.getCreatedAt)
-    val end = sortedPosts.map(sortedPost => sortedPost.reverse.head.getCreatedAt)
+  def getAverageTweetsPerDay(posts: Future[List[CustomTweet]]): Future[Long] = {
+    val sortedPosts = posts.map(post => post.sortWith((a, b) => a.createdAt.before(b.createdAt)))
+    val start = sortedPosts.map(sortedPost => sortedPost.head.createdAt)
+    val end = sortedPosts.map(sortedPost => sortedPost.reverse.head.createdAt)
     val localStart = start.map(start => new Timestamp(start.getTime).toLocalDateTime)
     val localEnd = end.map(end => new Timestamp(end.getTime).toLocalDateTime)
     val duration = localEnd.map(localEnd =>
